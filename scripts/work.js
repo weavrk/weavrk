2126debc1edfd3a1114ad211e6529a456045639b
weavrk <weavrk@gmail.com>
2022-07-01 09:26:07 -0500

    starting the day

--- scripts/work.js
+++ scripts/work.js
@@ -2,13 +2,13 @@
 const openMenu = document.getElementById("carrot");
 openMenu.addEventListener("click", showLinks);
 
-function showLinks () {
-	document.querySelector(".nav_links").classList.toggle("nav_links_show");
+function showLinks() {
+  document.querySelector(".nav_links").classList.toggle("nav_links_show");
 
-	if(document.getElementById("carrot").className == "caret_down")
-		document.getElementById("carrot").className = "caret_up";
-	else
-		document.getElementById("carrot").className = "caret_down";
+  if (document.getElementById("carrot").className == "caret_down")
+    document.getElementById("carrot").className = "caret_up";
+  else
+    document.getElementById("carrot").className = "caret_down";
 }
 
 
@@ -17,23 +17,23 @@ function showLinks () {
 // show info
 
 const elements = document.getElementsByClassName('show-info');
-for(let i=0; i<elements.length; i++) { 
+for (let i = 0; i < elements.length; i++) {
   elements[i].addEventListener("click", showProject);
 }
 
-function showProject()  {
-	const info = document.getElementsByClassName('grid_overlay');
-  for(let i=0; i<info.length; i++) { 
+function showProject() {
+  const info = document.getElementsByClassName('grid_overlay');
+  for (let i = 0; i < info.length; i++) {
     // info[i].classList.add("toggleProjectInfo");
     info[i].style.transform = "translateY(0%)";
-}
+  }
 }
 
 // hide info
 
 const elements2 = document.getElementsByClassName('hide-info');
-for(let i=0; i<elements2.length; i++) { 
-  elements2[i].addEventListener("click", function(){ location.reload(); });
+for (let i = 0; i < elements2.length; i++) {
+  elements2[i].addEventListener("click", function () { location.reload(); });
 }
 
 
